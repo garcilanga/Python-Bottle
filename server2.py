f3a5c806e503d091e88b4067c7cd506d77b64130
Juan García <33761598+garcilanga@users.noreply.github.com>
2017-12-01 21:35:44 +0100

    Update server2.py

--- server2.py
+++ server2.py
@@ -1,9 +1,11 @@
-from bottle import route, run
+from bottle import route, run, template
 
 @route('/')
 @route('/hello/<name>')
 def hello(name='Stranger'):
-    return '<h1>Hello %s, how are you?</h1>' % name
+    return template('<h1>Hello {{name}}, how are you?</h1>!', name=name)
 
 if __name__ == '__main__':
     run(host='localhost', port=8080, debug=True)
+
+    
